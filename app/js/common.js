27ad5db1f37a4b4b018ce38dc5f11b75008f63ba
wmayer <wmayer@yandex.ru>
2018-12-05 16:31:05 +0300

    My first commit

---
+++ app/js/common.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let persone = {
+  name: "John",
+  age: 25,
+  isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[2])
+
+
+alert("hello world")
